88802318bdad497c3b1301ce05138b607f3a3b21
Kevin Davis <dawnpatrol506@gmail.com>
2018-10-22 19:53:52 -0700

    trying to fix port

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 8080;
 const apiRouter = require('./app/routing/apiRoutes');
 const htmlRouter = require('./app/routing/htmlRoutes');
 
+
+app.set('port', (process.env.PORT || 8080))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -13,4 +14,4 @@ app.use('/', htmlRouter);
 app.use('*', htmlRouter);
 
 
-app.listen(port, () => console.log(`Listenting on port ${port}`));
+app.listen(app.get('port'), () => console.log(`Listenting on port ${port}`));
